37ccb35c2c5b81963cb1bd26198c71f6fdad73ee
jewpi <jewpi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-04 11:33:54 +0000

    svn update
    git-svn-id: https://plugins.svn.wordpress.org/thumbmaster/trunk@484414 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- thumbs.php
+++ thumbs.php
@@ -1,7 +1,7 @@
 <?
 /*
 Plugin Name: ThumbMaster
-Version: 0.1
+Version: 0.11
 Plugin URI: http://wordpress.org/extend/plugins/thumbmaster/
 Description: Generates properly formatted post thumbnails on-the-fly for plugins and themes. Fallback thumbnails, external images, Youtube videos supported.
 Author: Nathan Schlesinger
